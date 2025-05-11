9197d3f679366d3513dfcb3a937e00ab75a91566
hushin <ngtv.hsn@gmail.com>
2025-05-11 03:35:09 +0000

    feat: miseのインストールスクリプトを更新し、config.tomlを追加

--- home/.chezmoiscripts/linux/run_once_01_install-packages.sh
+++ home/.chezmoiscripts/linux/run_once_01_install-packages.sh
@@ -5,16 +5,20 @@ has() {
   type "$1" > /dev/null 2>&1
 }
 
+sudo apt update
 # mise
 if ! has "mise"; then
   echo "innstall mise"
   curl https://mise.run | sh
+  # for ruby
+  sudo apt-get install libyaml-dev
 fi
+mise self-update
+mise install
 
 # fish
 if ! has "fish"; then
   echo "install fish"
-  sudo apt update
   sudo apt install -y software-properties-common
   sudo add-apt-repository -y ppa:fish-shell/release-4
   sudo apt update

9f674f15c5c979e0809b56c9851f82b33b5a23f9
hushin <ngtv.hsn@gmail.com>
2025-05-12 21:56:16 +0900

    1ファイルに統合

---
+++ home/.chezmoiscripts/2_fish/run_once_after_01_setup.fish
@@ -0,0 +1,18 @@
+#!/usr/bin/fish
+
+function has
+  type "$argv[1]" > /dev/null 2>&1
+end
+
+# fisher
+if not has "fisher"
+    echo "install fisher"
+    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
+end
+echo "install fish plugins"
+fisher update
+
+echo "update fish completions"
+mise completion fish > ~/.config/fish/completions/mise.fish
+fish_update_completions
+
--- home/.chezmoiscripts/2_fish/run_once_after_02_update-packages.fish
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/fish
-
-echo "install fish plugins"
-fisher update
-
-echo "update fish completions"
-mise completion fish > ~/.config/fish/completions/mise.fish
-fish_update_completions

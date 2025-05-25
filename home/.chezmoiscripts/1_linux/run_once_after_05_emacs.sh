fdc8c8e7b08240909c1d70cd58b1b889d74a9fab
hushin <ngtv.hsn@gmail.com>
2025-05-25 21:24:30 +0900

    Brew emacs に切り替え

--- home/.chezmoiscripts/1_linux/run_once_after_05_emacs.sh
+++ home/.chezmoiscripts/1_linux/run_once_after_05_emacs.sh
@@ -5,12 +5,4 @@ has() {
   type "$1" > /dev/null 2>&1
 }
 
-# if ! has "emacs"; then
-#   # https://launchpad.net/~ubuntuhandbook1/+archive/ubuntu/emacs
-#   echo "install emacs"
-#   sudo add-apt-repository -y ppa:ubuntuhandbook1/emacs
-#   sudo apt update
-#   sudo apt install -y emacs-nox
-# fi
-
-# ~/.config/emacs/bin/doom install
+~/.config/emacs/bin/doom install

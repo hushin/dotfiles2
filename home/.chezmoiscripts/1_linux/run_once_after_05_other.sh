8a50c14a3ac6cf3ae6204955d3e9eb65e570de3a
hushin <ngtv.hsn@gmail.com>
2025-05-25 21:34:51 +0900

    fetchmd インストール

---
+++ home/.chezmoiscripts/1_linux/run_once_after_05_other.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+
+has() {
+  type "$1" > /dev/null 2>&1
+}
+
+if ! has "fetchmd"; then
+  echo "install fetchmd"
+  mkdir -p "$HOME/.local/bin"
+  curl -fsSL https://raw.githubusercontent.com/hushin/fetchmd/main/scripts/install.sh | bash
+fi

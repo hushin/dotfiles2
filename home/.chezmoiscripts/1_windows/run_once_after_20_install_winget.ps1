7e5f72fc5052dc2027b23bddc3ceed000e8cf4a5
hushin <ngtv.hsn@gmail.com>
2025-07-21 11:47:06 +0900

    feat: wingetインストールスクリプトをパッケージリストに変更

--- home/.chezmoiscripts/1_windows/run_once_after_20_install_winget.ps1
+++ home/.chezmoiscripts/1_windows/run_once_after_20_install_winget.ps1
@@ -1,13 +1,20 @@
-﻿winget install -e --id AutoHotkey.AutoHotkey
-winget install -e --id AgileBits.1Password
-winget install -e --id AgileBits.1Password.CLI
-winget install -e --id Google.Chrome
-winget install -e --id Google.JapaneseIME
-winget install -e --id Microsoft.VisualStudioCode
-winget install -e --id Microsoft.PowerShell
-winget install -e --id Microsoft.PowerToys
-# winget install -e --id Microsoft.PCManager
-winget install -e --id Dropbox.Dropbox
-winget install -e --id MartiCliment.UniGetUI --source winget
-winget install -e --id jdx.mise
-winget install -e --id Starship.Starship
+﻿$packages = @(
+  "AutoHotkey.AutoHotkey",
+  "AgileBits.1Password",
+  "AgileBits.1Password.CLI",
+  "Google.Chrome",
+  "Google.JapaneseIME",
+  "Microsoft.VisualStudioCode",
+  "Microsoft.PowerShell",
+  "Microsoft.PowerToys",
+  # "Microsoft.PCManager",
+  "Dropbox.Dropbox",
+  "MartiCliment.UniGetUI",
+  "jdx.mise",
+  "Starship.Starship",
+  "rsteube.Carapace"
+)
+
+foreach ($package in $packages) {
+  winget install -e --id $package --source winget
+}

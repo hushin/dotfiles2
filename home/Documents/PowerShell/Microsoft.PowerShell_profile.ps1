908739507525dacdee45a3750c02ef96bcbd8e8c
hushin <ngtv.hsn@gmail.com>
2025-07-21 12:21:08 +0900

    feat: PowerShellプロファイルをマージ

--- home/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ home/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,28 @@
+# $PROFILE.CurrentUserCurrentHost
 
+# 現在のセッションの PATH 情報を更新するための関数
+function Update-PathVariable {
+  $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") +
+  ";" +
+  [System.Environment]::GetEnvironmentVariable("Path", "User")
+}
+
+# miseのshimパスを追加
 $shimPath = "$env:USERPROFILE\AppData\Local\mise\shims"
 $currentPath = [Environment]::GetEnvironmentVariable('Path', 'User')
 $newPath = $currentPath + ";" + $shimPath
 [Environment]::SetEnvironmentVariable('Path', $newPath, 'User')
 
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
 
+# carapace (補完ツール) の設定
+$env:CARAPACE_BRIDGES = "zsh,fish,bash,inshellisense" # optional
+Set-PSReadLineOption -Colors @{ "Selection" = "`e[7m" }
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+carapace _carapace | Out-String | Invoke-Expression
 
-
+# zoxide (cdの改良) の初期化
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
 # Starship の初期化 (最後に追加)
 Invoke-Expression (&starship init powershell)

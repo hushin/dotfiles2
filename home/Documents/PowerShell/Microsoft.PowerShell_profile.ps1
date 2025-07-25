e3197f06ca3fd6f770028563d8c8749588ff3b32
hushin <ngtv.hsn@gmail.com>
2025-07-26 08:00:13 +0900

    feat: windows abbr 追加

--- home/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ home/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -105,6 +105,115 @@ function which($cmdname) {
     Get-Command $cmdname | Select-Object -ExpandProperty Definition
 }
 
+# Abbreviation system (fish-like abbr)
+$global:Abbreviations = @{}
+
+function abbr {
+    param(
+        [string]$Key,
+        [string]$Value
+    )
+
+    if (-not $Key) {
+        # List all abbreviations
+        foreach ($abbr in $global:Abbreviations.GetEnumerator()) {
+            Write-Output "$($abbr.Key) -> $($abbr.Value)"
+        }
+        return
+    }
+
+    if (-not $Value) {
+        # Show specific abbreviation
+        if ($global:Abbreviations.ContainsKey($Key)) {
+            Write-Output "$Key -> $($global:Abbreviations[$Key])"
+        }
+        else {
+            Write-Output "Abbreviation '$Key' not found"
+        }
+        return
+    }
+
+    # Add abbreviation
+    $global:Abbreviations[$Key] = $Value
+    # Write-Output "Added abbreviation: $Key -> $Value"
+}
+
+# Function to handle abbreviation expansion
+function Expand-Abbreviation {
+    $line = $null
+    $cursor = $null
+    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+
+    # Get the word before cursor
+    $words = $line.Substring(0, $cursor) -split '\s+'
+    $lastWord = $words[-1]
+
+    if ($global:Abbreviations.ContainsKey($lastWord)) {
+        # Replace the abbreviation
+        $replacement = $global:Abbreviations[$lastWord]
+        $startPos = $cursor - $lastWord.Length
+        [Microsoft.PowerShell.PSConsoleReadLine]::Replace($startPos, $lastWord.Length, $replacement)
+    }
+}
+
+# PSReadLine handler for abbreviation expansion on Space
+Set-PSReadLineKeyHandler -Key ' ' -ScriptBlock {
+    Expand-Abbreviation
+    # Insert the space
+    [Microsoft.PowerShell.PSConsoleReadLine]::Insert(' ')
+}
+
+# PSReadLine handler for abbreviation expansion on Enter
+Set-PSReadLineKeyHandler -Key 'Enter' -ScriptBlock {
+    Expand-Abbreviation
+    # Execute the command
+    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+}
+
+# Git abbreviations
+abbr 'ta' 'tig --all'
+abbr 'ts' 'tig status'
+abbr 'g' 'git'
+abbr 'gsh' 'git show'
+abbr 'gdc' 'git diff --cached'
+abbr 'gs' 'git switch'
+abbr 'gsc' 'git switch -c'
+
+# Search abbreviations
+abbr 'rgh' 'rg --hidden'
+
+# Chezmoi abbreviations
+abbr 'ch' 'chezmoi'
+abbr 'cha' 'chezmoi add'
+abbr 'che' 'chezmoi edit'
+abbr 'chm' 'chezmoi merge'
+abbr 'chd' 'chezmoi diff'
+abbr 'chp' 'chezmoi apply'
+abbr 'chu' 'chezmoi update'
+
+# Utility abbreviations
+abbr 'reload' '. $PROFILE'
+abbr 'diff' 'delta'
+abbr 'find' 'fd'
+abbr 'cdu' 'cd (git rev-parse --show-toplevel)'
+abbr 'tree' 'tree -N'
+abbr 'notes' 'rg "TODO|HACK|FIXME|OPTIMIZE"'
+
+# Editor abbreviations (Windows adapted)
+abbr 'e' 'code'
+abbr 'o' 'Invoke-Item'
+
+# eza abbreviations (if available)
+if (Get-Command eza -ErrorAction SilentlyContinue) {
+    abbr 'ls' 'eza --icons'
+    abbr 'll' 'eza --icons -lhag --time-style long-iso'
+    abbr 'lt' 'eza --icons --tree'
+}
+else {
+    abbr 'll' 'ls -la'
+    abbr 'la' 'ls -a'
+}
+
 # key binding
 # 実行後入力待ちになるため、AcceptLine を実行する
 Set-PSReadLineKeyHandler -Chord 'Ctrl+]' -ScriptBlock { gf; [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine() }

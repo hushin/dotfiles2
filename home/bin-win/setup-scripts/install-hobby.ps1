0167fe1d92a943513e9c619675d55d9b2d1bd681
hushin <ngtv.hsn@gmail.com>
2025-07-26 23:46:49 +0900

    feat: LINE Desktop, Kindleのインストールをスクリプトに追加

--- home/bin-win/setup-scripts/install-hobby.ps1
+++ home/bin-win/setup-scripts/install-hobby.ps1
@@ -1,12 +1,14 @@
 ﻿$packages = @(
-    "LINE.LINE",
     "Discord.Discord",
+    "Amazon.Kindle",
     "Valve.Steam",
     "MPC-BE.MPC-BE",
     "VideoLAN.VLC",
     "neelabo.NeeView",
     "Philips.HueSync"
 )
+
+winget install -e --id XPFCC4CD725961 # LINE Desktop
 
 foreach ($package in $packages) {
     winget install -e --id $package --source winget

79fd9a971d54abc2c9280cdb21f3d9fce00b89f9
hushin <ngtv.hsn@gmail.com>
2025-08-02 20:13:21 +0900

    mac でも動くようにsh設定修正

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -5,12 +5,19 @@ end
 if type -q fenv
     fenv source ~/.profile
 end
-~/.local/bin/mise activate fish | source
-starship init fish | source
+
+if command -v mise >/dev/null 2>&1
+    mise activate fish | source
+end
+if command -v starship >/dev/null 2>&1
+    starship init fish | source
+end
 
 . ~/.config/fish/env.fish
 . ~/.config/fish/aliases.fish
 . ~/.config/fish/funcs.fish
 . ~/.config/fish/keybinds.fish
 
-zoxide init fish | source
+if command -v zoxide >/dev/null 2>&1
+    zoxide init fish | source
+end

bcc83e86fef04b7d7915f6eb8eb062d4f3160af1
hushin <ngtv.hsn@gmail.com>
2025-08-04 00:03:32 +0900

    fix: Emacs.appのエイリアス作成処理を条件付きで実行するように修正

--- home/.chezmoiscripts/1_darwin/run_once_after_05_emacs.sh
+++ home/.chezmoiscripts/1_darwin/run_once_after_05_emacs.sh
@@ -7,4 +7,7 @@ echo "install doom emacs"
 ~/.config/emacs/bin/doom install
 ~/.config/emacs/bin/doom sync
 
-# osascript -e 'tell application "Finder" to make alias file to posix file "/opt/homebrew/opt/emacs-plus@30/Emacs.app" at posix file "/Applications" with properties {name:"Emacs.app"}'
+if [ ! -e "/Applications/Emacs.app" ]; then
+    echo "Creating Emacs.app alias in /Applications"
+    osascript -e 'tell application "Finder" to make alias file to posix file "/opt/homebrew/opt/emacs-plus@30/Emacs.app" at posix file "/Applications" with properties {name:"Emacs.app"}'
+fi

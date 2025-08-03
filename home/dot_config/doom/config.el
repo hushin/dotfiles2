bb3e62adc522af2ce8108b15deca6d133ac1dd19
hushin <ngtv.hsn@gmail.com>
2025-08-03 14:16:08 +0900

    今までの設定を復元

--- home/dot_config/doom/config.el
+++ home/dot_config/doom/config.el
@@ -75,3 +75,199 @@
 ;;
 ;; 'gd'（または 'C-c c d'）を試して定義にジャンプし、実装方法を確認することもできます。
 
+
+;; Ctrl-h
+;(map! "C-h" 'delete-backward-char)
+
+;; delete character without yanking
+(map! :n "x" 'delete-char)
+
+;; leader key
+(add-hook! 'org-mode-hook #'+org-init-keybinds-h)
+(setq evil-snipe-override-evil-repeat-keys nil)
+(setq doom-localleader-key ",")
+(setq doom-localleader-alt-key "M-,")
+
+;; auto save
+(use-package! super-save
+  :config
+  (setq super-save-auto-save-when-idle t
+    super-save-idle-duration 1)
+  (super-save-mode +1)
+  )
+
+;; Disable exit confirmation.
+(setq confirm-kill-emacs nil)
+
+;; org-mode の日付を英語にする
+(setq system-time-locale "C")
+;; UTF-8をデフォルトのエンコーディングとして設定
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(when (featurep :system 'windows)
+  ;; Windows 用のクリップボード設定
+  (set-selection-coding-system 'utf-16le-dos)
+  ;; Windows search-project で 日本語で検索できるようにする
+  (defun advice:with-japanese-coding-system (orig-fun &rest args)
+    (let ((coding-system-for-write 'cp932))
+      (apply orig-fun args)))
+  (advice-add '+default/search-project :around 'advice:with-japanese-coding-system))
+
+(setq calendar-week-start-day 1)
+;; Displaying week numbers in calendar
+(copy-face font-lock-constant-face 'calendar-iso-week-face)
+(set-face-attribute 'calendar-iso-week-face nil
+                    :height 1.0
+                    :foreground "gray50")
+(setq calendar-intermonth-text
+  '(propertize
+     (format "w%2d"
+       (car
+         (calendar-iso-from-absolute
+           (calendar-absolute-from-gregorian
+             (list month (- day (1- calendar-week-start-day)) year)))))
+     'font-lock-face 'calendar-iso-week-face))
+
+(setq markdown-fontify-code-blocks-natively t)
+
+;; ファイル操作関数
+(load! "lib/file-operations")
+
+;; org-agenda 設定
+(load! "lib/org-agenda-config")
+
+(after! org
+  (map!
+    "C-c a" #'org-agenda
+    "C-c c" #'org-capture
+    "C-c j" #'org-journal-new-entry
+    )
+  (setq org-todo-keywords
+    (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+             (sequence "WAITING(w/!)" "|" "CANCELED(c/!)"))))
+  (setq org-log-done 'time)
+  ; 見出し入れるときは空行を入れない
+  (setq org-blank-before-new-entry '((heading . nil) (plain-list-item . nil)))
+  (defun my/property-values-function (property)
+    "Return allowed values for PROPERTY."
+    (cond
+      ((string= property "Type")
+        '("Book" "Web" "Anime" "Game" "Podcast" "Video" "Movie"))
+      ((string= property "Rating")
+        '("*" "**" "***" "****" "*****"))
+      ((string= property "Canceled")
+        '("true" ""))
+      ))
+
+  (setq org-property-allowed-value-functions
+    '(my/property-values-function))
+
+  (setq org-capture-templates
+    '(
+       ("t" "Task" entry (file+headline "gtd.org" "Inbox")
+         "* TODO %? \nCREATED: %U\n %i")
+       ("n" "Task NEXT" entry (file+headline "gtd.org" "Inbox")
+         "* NEXT %? \nCREATED: %U\n %i ")
+       ("T" "Task from protocol" entry (file+headline "gtd.org" "Inbox")
+         "* TODO %? [[%:link][%:description]] \nCREATED: %U\n%i\n\n")
+       ("L" "ReadItLater" entry (file+headline "gtd.org" "ReadItLater")
+         "* TODO %? [[%:link][%:description]] \nCREATED: %U\n%i\n")
+       ))
+
+
+  ;; journal
+  (setq org-journal-file-format "%Y-%m-%d")
+  (setq org-journal-date-format "%Y-%m-%d %A")
+  ;; (setq org-journal-time-format "%R ")
+  (setq org-journal-file-type 'weekly)
+  (setq org-journal-find-file 'find-file)
+  (setq org-extend-today-until '3)
+  (add-hook 'org-journal-after-entry-create-hook 'evil-insert-state)
+  (setq org-startup-with-inline-images t)
+
+  (setq org-M-RET-may-split-line '((item . t)))
+  ;; export周りの設定
+  (setq org-export-with-toc nil)
+  (setq org-export-with-section-numbers nil)
+  (setq org-export-with-creator nil)
+  (setq org-use-sub-superscripts nil)
+  (setq org-export-with-sub-superscripts nil)
+
+  (load! "lib/task-collection")
+  (load! "lib/org-bullet-functions")
+  )
+
+(map! :after evil-org
+  :map evil-org-mode-map
+  :ni "C-<return>" #'org-insert-heading-respect-content
+  :ni "C-S-<return>" #'org-insert-todo-heading-respect-content
+  :ni "M-<left>" #'org-metaleft
+  :ni "M-<right>" #'org-metaright
+  )
+
+(after! evil-org
+  (remove-hook 'org-tab-first-hook #'+org-cycle-only-current-subtree-h)
+  )
+
+(after! org-roam
+  (map!
+    "C-c n l" #'org-roam-buffer-toggle
+    "C-c n f" #'org-roam-node-find
+    "C-c l" #'org-roam-dailies-goto-today
+    "C-c d" #'org-roam-dailies-map
+    :n "[ D" #'org-roam-dailies-goto-previous-note
+    :n "] D" #'org-roam-dailies-goto-next-note
+    :map org-mode-map
+    "C-c n i" #'org-roam-node-insert
+    "C-M-i" #'completion-at-point
+    )
+  (setq org-roam-completion-everywhere nil)
+
+  (setq org-roam-node-display-template
+    (format "%s ${doom-hierarchy:*} %s"
+      (propertize "${doom-type:15}" 'face 'font-lock-keyword-face)
+      (propertize "${doom-tags:10}" 'face '(:inherit org-tag :box nil))))
+
+  (load! "lib/org-roam-templates")
+  (load! "lib/weekly-review")
+  (load! "lib/org-timestamp")
+  (load! "lib/org-file-browser")
+  )
+
+(after! org-roam-protocol
+  (defun org-roam-protocol-open-ref (info)
+    "Process an org-protocol://roam-ref?ref= style url with INFO."
+    (let ((org-capture-link-is-already-stored t))
+      (org-roam-capture-
+        :keys (plist-get info :template)
+        :node (org-roam-node-create :title (plist-get info :title))
+        :info (list :ref (plist-get info :ref)
+                :body (plist-get info :body)
+                ;; 独自の変数を送れるように追加
+                :type (plist-get info :type)
+                :creator (plist-get info :creator)
+                :releaseDate (plist-get info :releaseDate)
+                )
+        :templates org-roam-capture-ref-templates))
+    nil)
+  )
+
+(after! org-download
+  (when (winp)
+    (setq org-download-screenshot-method "powershell -c Add-Type -AssemblyName System.Windows.Forms;$image = [Windows.Forms.Clipboard]::GetImage();$image.Save('%s', [System.Drawing.Imaging.ImageFormat]::Png)")
+    )
+  )
+
+(use-package! org-super-agenda
+  :after org-agenda
+  :hook (org-agenda-mode . org-super-agenda-mode)
+  :config
+  ;; evil keymap https://github.com/alphapapa/org-super-agenda/issues/50
+  (setq org-super-agenda-header-map (make-sparse-keymap))
+  )
+
+(use-package! org-roam-timestamps
+  :after org-roam
+  :config (org-roam-timestamps-mode))

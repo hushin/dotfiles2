1f54b436c1c72f9a5c6c5b88df9f790e571db2bb
hushin <ngtv.hsn@gmail.com>
2025-10-25 11:07:34 +0900

    kiro の設定追加

--- home/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ home/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -3,3 +3,5 @@ function Update-PathVariable {
     $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" +
     [System.Environment]::GetEnvironmentVariable("Path", "User")
 }
+
+if ($env:TERM_PROGRAM -eq "kiro") { . "$(kiro --locate-shell-integration-path pwsh)" }

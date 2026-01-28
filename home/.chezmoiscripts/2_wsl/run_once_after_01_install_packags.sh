cbba2cffe43d275bc152673ad9bf37c6b995e42b
hushin <ngtv.hsn@gmail.com>
2026-01-28 23:36:06 +0900

    feat: WSLのデフォルトブラウザをwslviewからexplorer.exeに変更。wslu はアーカイブされていたので削除

--- home/.chezmoiscripts/2_wsl/run_once_after_01_install_packags.sh
+++ home/.chezmoiscripts/2_wsl/run_once_after_01_install_packags.sh
@@ -4,11 +4,6 @@ set -euo pipefail
 has() {
     type "$1" > /dev/null 2>&1
 }
-# wslu
-if ! has "wslview"; then
-    echo "install wslu"
-    sudo apt install -y wslu
-fi
 
 # xsel
 if ! has "xsel"; then
